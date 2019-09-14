bddedd4349d84326fdad79052955fc43250ea60f
YU-HSIN, CHEN <s63232@gmail.com>
2019-09-14 18:27:14 +0800

    feat: Add restaurantSeeder to generate mock data

---
+++ models/restaurant.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+// 製作 mongoose model
+const restaurantSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  name_en: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  google_map: {
+    type: String
+  },
+  rating: {
+    type: Number
+  },
+  description: {
+    type: String
+  }
+})
+
+const Restaurant = mongoose.model('restaurant', restaurantSchema)
+
+module.exports = Restaurant
---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant.js')
+const restaurantsData = require('./restaurants.json')
+
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true })
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'mongoDB error'))
+db.once('open', () => {
+  console.log('mongoDB is connected.')
+  const {results}=restaurantsData
+  results.forEach(result=>Restaurant.create(result))
+})
